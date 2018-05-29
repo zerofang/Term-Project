94888daf6859dff1bfcd1ee96f72f1bee65d25a4
zerofang <zerofangzerofang@outlook.com>
2018-05-29 15:21:03 +0800

    精简

--- Network-Computing/student-management-system/index.php
+++ Network-Computing/student-management-system/index.php
@@ -58,62 +58,12 @@ function myFunction(xml) {
 }
 </script>
 <?php
-$host = 'localhost';//填服务器名
-$user = '';//填数据库用户名
-$pass = '';//填数据库密码
-$name = '';//填数据库名字
+$host = 'localhost';
+$user = 'your username';//此处替换为你自己的数据库用户名，密码，数据库名
+$pass = 'your password';
+$name = 'database name';
 
-require("database.php");
-function getConstraints($table, $columnName)
-{
-    $getConstraints = 'SELECT cols.TABLE_NAME AS INITIAL_TABLE_NAME, cols.COLUMN_NAME AS INITIAL_COLUMN_NAME, cols.ORDINAL_POSITION,
-  cols.COLUMN_DEFAULT, cols.IS_NULLABLE, cols.DATA_TYPE,
-  cols.CHARACTER_MAXIMUM_LENGTH, cols.CHARACTER_OCTET_LENGTH,
-  cols.NUMERIC_PRECISION, cols.NUMERIC_SCALE,
-  cols.COLUMN_TYPE, cols.COLUMN_KEY, cols.EXTRA,
-  cols.COLUMN_COMMENT, refs.REFERENCED_TABLE_NAME, refs.REFERENCED_COLUMN_NAME,
-  cRefs.UPDATE_RULE, cRefs.DELETE_RULE,
-  links.TABLE_NAME, links.COLUMN_NAME,
-  cLinks.UPDATE_RULE, cLinks.DELETE_RULE
-  FROM INFORMATION_SCHEMA.`COLUMNS` as cols
-  LEFT JOIN INFORMATION_SCHEMA.`KEY_COLUMN_USAGE` AS refs
-  ON refs.TABLE_SCHEMA=cols.TABLE_SCHEMA
-  AND refs.REFERENCED_TABLE_SCHEMA=cols.TABLE_SCHEMA
-  AND refs.TABLE_NAME=cols.TABLE_NAME
-  AND refs.COLUMN_NAME=cols.COLUMN_NAME
-  LEFT JOIN INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS AS cRefs
-  ON cRefs.CONSTRAINT_SCHEMA=cols.TABLE_SCHEMA
-  AND cRefs.CONSTRAINT_NAME=refs.CONSTRAINT_NAME
-  LEFT JOIN INFORMATION_SCHEMA.`KEY_COLUMN_USAGE` AS links
-  ON links.TABLE_SCHEMA=cols.TABLE_SCHEMA
-  AND links.REFERENCED_TABLE_SCHEMA=cols.TABLE_SCHEMA
-  AND links.REFERENCED_TABLE_NAME=cols.TABLE_NAME
-  AND links.REFERENCED_COLUMN_NAME=cols.COLUMN_NAME
-  LEFT JOIN INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS AS cLinks
-  ON cLinks.CONSTRAINT_SCHEMA=cols.TABLE_SCHEMA
-  AND cLinks.CONSTRAINT_NAME=links.CONSTRAINT_NAME
-  WHERE cols.TABLE_SCHEMA= \'mydb\'
-  AND cols.TABLE_NAME= ?
-  AND cols.COLUMN_NAME= ?;';
 
-    $pdo = Database::connect();
-    $q1 = $pdo->prepare($getConstraints);
-    $q1->execute(array($table,$columnName));
-    $constraints = null;
-    foreach ($q1->fetchAll() as $row) {
-        if ($columnName==$row['INITIAL_COLUMN_NAME']) {
-            $constraints = array(
-              0 => $row['IS_NULLABLE'],
-              1 => $row['COLUMN_TYPE'],
-              2 => $row['COLUMN_KEY'],
-              3 => $row['EXTRA'],
-              4 => $row['REFERENCED_TABLE_NAME'],
-              5 => $row['REFERENCED_COLUMN_NAME'],
-            );
-        }
-    }
-    return $constraints;
-}
 
 //connect
 $link = mysqli_connect($host, $user, $pass);
